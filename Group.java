1b31a23564ccd6e2fac097a60c8aff23945ef4c6
Ethan <ethansaddlerm@gmail.com>
2022-10-25 15:28:51 -0400

    Group changes

--- Group.java
+++ Group.java
@@ -1,1 +1,14 @@
+public class Group {
+    protected String[] people;
 
+    public Group(String[] people) {
+        String[] newPeople;
+        if (people == null) {
+            newPeople = new String[0];
+        }
+        newPeople = new String[people.length];
+        for (int n = 0 ; n < people.length ; n++) {
+            newPeople[n] = people[n];
+        }
+    }
+}
