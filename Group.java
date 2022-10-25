586cde79e771f4a30593ec88dd75d5671a3542fd
Ethan <ethansaddlerm@gmail.com>
2022-10-25 16:08:59 -0400

    implimented group and admittable classes

--- Group.java
+++ Group.java
@@ -10,5 +10,21 @@ public class Group {
         for (int n = 0 ; n < people.length ; n++) {
             newPeople[n] = people[n];
         }
+        this.people =  newPeople;
+    }
+    public int size(Group group) {
+        return group.people.length;
+    }
+    @Override
+    public String toString() {
+        String str = "";
+        for (int n = 0 ; n < this.people.length ; n++) {
+            if (n == 0) {
+                System.out.print(people[n]);
+                continue;
+            }
+            str += String.format("/%s", people[n]);
+        }
+        return str;
     }
 }
